8021f75863dacc8b4f02aec4b40c2671ba519be5
RyzenJS <ryzenjs@gmail.com>
2021-02-13 00:59:18 +0800

    +function rules +function updatedTitle +function create +function modelData +function resetVars +priv function generateSlug

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Page;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class Pages extends Component
@@ -14,6 +15,47 @@ class Pages extends Component
 
         
     /**
+     * Validation rules
+     *
+     * @return void
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('pages', 'slug')],
+            'content' => 'required',
+        ];
+    }
+
+    
+    /**
+     * Runs everytime if the title
+     * variable is being updated
+     *
+     * @param  mixed $value
+     * @return void
+     */
+    public function updatedTitle($value)
+    {
+        $this->generateSlug($value);
+    }
+
+
+    /**
+     * create function
+     *
+     * @return void
+     */
+    public function create()
+    {
+        $this->validate();
+        Page::create($this->modelData());
+        $this->modalFormVisible = false;
+        $this->resetVars();
+    }
+        
+    /**
      * ctrl + shift i shortcut comment on function
      * Shows the form modal
      * of the creeate function.
@@ -23,10 +65,50 @@ class Pages extends Component
     {
         $this->modalFormVisible = true;
     }
+        
+    /**
+     * Data for the model mapped
+     *in this component
+     * @return void
+     */
+    public function modelData()
+    {
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+        ];
+    }
+
     
     /**
-     * ctrl + shift i shortcut comment on function
-     * livewire render function
+     * Resets variables to null
+     *
+     * @return void
+     */
+    public function resetVars()
+    {
+        $this->title = null;
+        $this->slug = null;
+        $this->content = null;
+    }
+    
+    /**
+     * Generates url slug based on the title
+     *
+     * @param  mixed $value
+     * @return void
+     */
+    private function generateSlug($value)
+    {
+        $process1 = str_replace(' ','-', $value);
+        $process2 = strtolower($process1);
+        $this->slug = $process2;
+    }
+
+     
+    /**
+     * live wire render function
      *
      * @return void
      */
